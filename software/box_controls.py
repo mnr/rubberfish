6c98bbcaa3fb4b842ec89b66eadd0940f176f6ab
Mark Niemann-Ross <mark.niemannross@gmail.com>
2016-06-05 19:57:50 -0700

    fixed omitted self

--- software/box_controls.py
+++ software/box_controls.py
@@ -16,8 +16,8 @@ class boxControls(object):
         self.arg = arg
 
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(boxVent, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(boxLIGHT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.boxVent, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.boxLIGHT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
         GPIO.add_event_detect(self.boxVENT, GPIO.BOTH, bouncetime=200)
         GPIO.add_event_detect(self.boxLIGHT, GPIO.BOTH, bouncetime=200)

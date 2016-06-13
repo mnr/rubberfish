6e72862945f65f981722f1e30956cb6ef90ce7d8
Mark Niemann-Ross <mark.niemannross@gmail.com>
2016-06-13 09:35:57 -0700

    added code for pyttsx support

--- software/bmbb_fish.py
+++ software/bmbb_fish.py
@@ -5,8 +5,9 @@ from time import sleep as sleep
 import urllib
 from urllib.parse import quote_plus
 from urllib.request import urlretrieve
-#import pyttsx
-#import wordToSay
+import pyttsx
+import wordToSay
+import uuid
 
 class BmBB:
     """ interface with the controls and motors of the big mouth billy bass """
@@ -22,8 +23,10 @@ class BmBB:
 
     # other variables
     PWMstatus = None #declaring PWMstatus here for later assignment
-    #SpeechEngine = None
-    #EngineDict = None # used to disconnect a callback
+    SpeechEngine = None
+    SpeechCallBack_startword = None # used to disconnect the callback to started-word
+    SpeechCallBack_finishutter = None # used to disconnect the callback to finished-utterance
+    SpeechWordObjects = []
 
 
     def __init__(self):
@@ -44,9 +47,10 @@ class BmBB:
         # set up text-to-speech engine
         # pyttsx documentation at http://pyttsx.readthedocs.io/en/latest/
         # python 3 requires this https://github.com/Julian-O/pyttsx
-        #self.SpeechEngine = pyttsx.init()
-        #self.SpeechEngine.setProperty('rate', 70)
-        #self.EngineDict = self.SpeechEngine.connect('started-word', self.flapMouth )
+        self.SpeechEngine = pyttsx.init()
+        self.SpeechEngine.setProperty('rate', 70)
+        self.SpeechCallBack_startword = self.SpeechEngine.connect('started-word', self.onStartWord )
+        self.SpeechCallBack_finishutter = self.SpeechEngine.connect('finished-utterance', self.onFinishUtterance )
 
         # do something to indicate life
         self.mouth()
@@ -55,7 +59,8 @@ class BmBB:
         if self.debugMode: print("killing the fish")
         self.PWMstatus.stop() # turn off PWM
         GPIO.cleanup() #resets the GPIO state to neutral
-        # self.SpeechEngine.disconnect(self.EngineDict) #disconnects the speech engine
+        self.SpeechEngine.disconnect(self.SpeechCallBack_startword) #disconnects a callback
+        self.SpeechEngine.disconnect(self.SpeechCallBack_finishutter) #disconnects a callback
 
     def mouth(self,fishDuration=.5,enthusiasm=75):
         if self.debugMode: print('mouth: duration={durate}, enthusiasm={enth}.'.format(durate=fishDuration, enth=enthusiasm))
@@ -84,13 +89,27 @@ class BmBB:
         PWMDutyCycle = 0 if PWMDutyCycle < 0 else PWMDutyCycle
         self.PWMstatus.ChangeDutyCycle(PWMDutyCycle)
 
-    def flapMouth(self,nameOfPhrase,locationNumber,lengthOfWord):
+    def onStartWord(self,nameOfPhrase,locationNumber,lengthOfWord):
         # flaps the mouth for each word * syllables in word
         # called by pyttsx as a call back routine
-        self.mouth()
+        thisWordObject = self.SpeechWordObjects[locationNumber]
+        """ I'm guessing that the "length" in onStartWord(name : string, location : integer, length : integer) is a measurement of time? Seconds? """
+        thisWordObject.setlengthOfWord(lengthOfWord)
+        syllableCounter = thisWordObject.wordSyllablesMax
+        while (syllableCounter):
+            self.mouth(fishDuration=thisWordObject.secondsPerSyllable)
+            sleep(thisWordObject.secondsPerSyllable)
+            syllableCounter -= 1
+
+    def onFinishUtterance(self,name, completed):
+        #name: Name associated with the utterance.
+        #completed: True if the utterance was output in its entirety or not.
+        self.SpeechWordObjects.clear()
 
     def speak(self,say_this_phrase):
-        # flapMouth() is set up as a word callback in the init
-        uniqueNameOfPhrase = "something" #create some unique name to id this phrase
+        # says the phrase, plus animates the fish mouth, head and tail in sync to speech
+        for aword in say_this_phrase.split():
+            self.SpeechWordObjects.append(new wordToSay(aword)) #create an object for each word
+        uniqueNameOfPhrase = uuid.uuid4() #create some unique name to id this phrase
         self.SpeechEngine.say(say_this_phrase, uniqueNameOfPhrase)
         self.SpeechEngine.runAndWait()

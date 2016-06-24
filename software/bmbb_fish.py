81479caa8b28d00408a4f6fdd5b1c06a898da203
Mark Niemann-Ross <mark.niemannross@gmail.com>
2016-06-24 09:15:16 -0700

    removed pyttsx in favor of watson

--- software/bmbb_fish.py
+++ software/bmbb_fish.py
@@ -5,8 +5,8 @@ from time import sleep as sleep
 import urllib
 from urllib.parse import quote_plus
 from urllib.request import urlretrieve
-import pyttsx
 import wordToSay
+import TextToFishSpeak
 import uuid
 
 class BmBB:
@@ -23,11 +23,7 @@ class BmBB:
 
     # other variables
     PWMstatus = None #declaring PWMstatus here for later assignment
-    SpeechEngine = None
-    SpeechCallBack_startword = None # used to disconnect the callback to started-word
-    SpeechCallBack_finishutter = None # used to disconnect the callback to finished-utterance
     SpeechWordObjects = []
-
 
     def __init__(self):
         GPIO.cleanup()
@@ -43,14 +39,6 @@ class BmBB:
         GPIO.setup(self.fishMotorEnable, GPIO.OUT)
         self.PWMstatus = GPIO.PWM(self.fishMotorEnable, 50) #frequency 50 hz
         self.PWMstatus.start(0) #duty cycle of zero. Enabled but silent
-
-        # set up text-to-speech engine
-        # pyttsx documentation at http://pyttsx.readthedocs.io/en/latest/
-        # python 3 requires this https://github.com/Julian-O/pyttsx
-        self.SpeechEngine = pyttsx.init()
-        self.SpeechEngine.setProperty('rate', 70)
-        self.SpeechCallBack_startword = self.SpeechEngine.connect('started-word', self.onStartWord )
-        self.SpeechCallBack_finishutter = self.SpeechEngine.connect('finished-utterance', self.onFinishUtterance )
 
         # do something to indicate life
         self.mouth()
@@ -89,27 +77,7 @@ class BmBB:
         PWMDutyCycle = 0 if PWMDutyCycle < 0 else PWMDutyCycle
         self.PWMstatus.ChangeDutyCycle(PWMDutyCycle)
 
-    def onStartWord(self,nameOfPhrase,locationNumber,lengthOfWord):
-        # flaps the mouth for each word * syllables in word
-        # called by pyttsx as a call back routine
-        thisWordObject = self.SpeechWordObjects[locationNumber]
-        """ I'm guessing that the "length" in onStartWord(name : string, location : integer, length : integer) is a measurement of time? Seconds? """
-        thisWordObject.setlengthOfWord(lengthOfWord)
-        syllableCounter = thisWordObject.wordSyllablesMax
-        while (syllableCounter):
-            self.mouth(fishDuration=thisWordObject.secondsPerSyllable)
-            sleep(thisWordObject.secondsPerSyllable)
-            syllableCounter -= 1
-
-    def onFinishUtterance(self,name, completed):
-        #name: Name associated with the utterance.
-        #completed: True if the utterance was output in its entirety or not.
-        self.SpeechWordObjects.clear()
-
     def speak(self,say_this_phrase):
         # says the phrase, plus animates the fish mouth, head and tail in sync to speech
         for aword in say_this_phrase.split():
-            self.SpeechWordObjects.append(new wordToSay(aword)) #create an object for each word
-        uniqueNameOfPhrase = uuid.uuid4() #create some unique name to id this phrase
-        self.SpeechEngine.say(say_this_phrase, uniqueNameOfPhrase)
-        self.SpeechEngine.runAndWait()
+            doTextToSpeech(new wordToSay(aword))

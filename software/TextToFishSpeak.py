94c0fc2bcbe82f06793dfabd784384fa22247966
Mark Niemann-Ross <mark.niemannross@gmail.com>
2016-06-24 14:53:19 -0700

    more major changes to watson tts

--- software/TextToFishSpeak.py
+++ software/TextToFishSpeak.py
@@ -1,47 +1,24 @@
 # this version of TextToFishSpeak uses Watson
 # see github.com/watson-developer-cloud/python-sdk
+# http://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/text-to-speech.html
+
+"""
+TextToFishSpeak provides a generic entry to a text to speech routine.
+This version implements watson
+"""
 
 import json
 import wordToSay
-from os.path import join, dirname
+from os.path import join, dirname, expanduser
 from watson_developer_cloud import TextToSpeechV1
 
-class TextToFishSpeak:
-    """
-    TextToFishSpeak provides a generic entry to a text to speech routine.
-    This version implements watson
-    http://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/text-to-speech.html
-    """
-
+def doTextToSpeech (self,stringObjectToSay="You forgot to tell me what to say"):
+    # stringObjectToSay is an object as created by wordToSay
     text_to_speech = TextToSpeechV1(
         username='2cf75972-4837-475a-a0ff-a646dfb94883',
         password='1nIH5xBrttWl')
 
-    def __init__(self):
-        pass
-
-
-
-    def doTextToSpeech (self,stringObjectToSay):
-        # stringObjectToSay is an object as created by wordToSay
-
-audio_file_path = join(os.path.expanduser('~'),'output.wav')
-audio_file = open(audio_file_path,'wb')
-audio_file.write(text_to_speech.synthesize('Hello world!',accept='audio/wav',voice="en-US_AllisonVoice"))
-
-# to play
-import pygame
-pygame.mixer.init()
-pygame.mixer.music.load(audio_file_path)
-pygame.mixer.music.play()
-
-        with open(join(os.path.expanduser('~'), 'output.wav'), 'wb') as audio_file:
-            audio_file.write(text_to_speech.synthesize('Hello world!'))
-
-        jsonData = {"con1":40, "con2":20, "con3":99, "con4":40, "password":"1234"}
-        URLforService = 'http://watson something'
-        params = json.dumps(jsonData).encode('utf8')
-        req = urllib.request.Request(URLforService, data=params,
-                             headers={'content-type': 'application/json'})
-        response = urllib.request.urlopen(req)
-        #print(response.read().decode('utf8'))
+    audio_file_path = join(expanduser('~'),'speechFromWatson.wav')
+    audio_file = open(audio_file_path,'wb')
+    audio_file.write(text_to_speech.synthesize(stringObjectToSay,accept='audio/wav',voice="en-US_AllisonVoice"))
+    return audio_file_path

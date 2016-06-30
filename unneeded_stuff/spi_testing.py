7b3d48d8716a532039dd1dd56ab7c0018caf80b9
Mark Niemann-Ross <mark.niemannross@gmail.com>
2016-06-29 18:46:55 -0700

    changing speed

--- unneeded_stuff/spi_testing.py
+++ unneeded_stuff/spi_testing.py
@@ -14,7 +14,7 @@ import time
 spi=spidev.SpiDev() # create a spi object
 
 spiBus = 0          # spi port 0
-spiSpeed = 20000
+spiSpeed = 200000
 spiDeviceCh = 0       # GPIO CE0
 spiDevice = spi.open(spiBus,spiDeviceCh)
 spi.max_speed_hz=spiSpeed # mcp3008 requires >10khz
@@ -25,10 +25,10 @@ spi.max_speed_hz=spiSpeed # mcp3008 requires >10khz
 spiStart = 0b00000001
 spiControl = 0b00001000 # single end mcp3008 ch0
 """ other spi control values to try
-0b1000 # single-end ch0
-0b1001 # single-end ch1
-0b0000 # differential ch0
-0b0001 # differential ch1
+0b1000 0000 # single-end ch0
+0b1001 0000 # single-end ch1
+0b0000 0000 # differential ch0 = in+
+0b0001 0000 # differential ch1 = in+
 """
 spiControlList = [0b10000000,0b10010000,0b00000000,0b00010000]
 spiPlaceholder = 0b00000000

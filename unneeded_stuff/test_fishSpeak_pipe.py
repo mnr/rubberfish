c2394845d0bc2242cfe648e1e710d24a83d6e7f1
MNR at the Fish <mark.niemannross@gmail.com>
2016-07-06 21:07:04 -0700

    fixed import time

--- unneeded_stuff/test_fishSpeak_pipe.py
+++ unneeded_stuff/test_fishSpeak_pipe.py
@@ -1,10 +1,11 @@
 import os
+from time import sleep as sleep
 
 FIFO_PATH = '/tmp/SayThis_Fish'
 
 pipeout = os.open(FIFO_PATH, os.O_WRONLY)
 counter = 0
 while True:
-    time.sleep(1)
+    sleep(1)
     os.write(pipeout, 'Hello, big Number %03d\n' % counter)
     counter = (counter+1) % 5

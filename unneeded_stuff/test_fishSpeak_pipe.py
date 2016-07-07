4c9ac456240c5ca5c566c2364fdbf3d635ed137e
Mark Niemann-Ross <mark.niemannross@gmail.com>
2016-07-06 21:38:29 -0700

    get rid of string variable

--- unneeded_stuff/test_fishSpeak_pipe.py
+++ unneeded_stuff/test_fishSpeak_pipe.py
@@ -4,6 +4,5 @@ FIFO_PATH = '/tmp/SayThis_Fish'
 
 pipeout = os.open(FIFO_PATH,os.O_WRONLY)
 
-saythis = "hello, I am a fish"
-os.write(pipeout, bytes(saythis, 'UTF-8'))
+os.write(pipeout, bytes("hello, I am a fish", 'UTF-8'))
 os.close(pipeout)

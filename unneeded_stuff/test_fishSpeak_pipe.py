e7d7991ac5fd79df30ad55ce02462071fe84e93b
Mark Niemann-Ross <mark.niemannross@gmail.com>
2016-07-06 21:15:31 -0700

    simplify

--- unneeded_stuff/test_fishSpeak_pipe.py
+++ unneeded_stuff/test_fishSpeak_pipe.py
@@ -1,11 +1,7 @@
 import os
-from time import sleep as sleep
 
 FIFO_PATH = '/tmp/SayThis_Fish'
 
-pipeout = os.open(FIFO_PATH, os.O_WRONLY)
-counter = 0
-while True:
-    sleep(1)
-    os.write(pipeout, 'Hello, big Number %03d\n' % counter)
-    counter = (counter+1) % 5
+pipeout = os.open(FIFO_PATH, 'w')
+
+os.write(pipeout, 'Hello, I am a fish')

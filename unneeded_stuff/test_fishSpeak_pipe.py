4994fdccdd7b98274a7393c5d28c782a7b469f59
Mark Niemann-Ross <mark.niemannross@gmail.com>
2016-07-06 21:34:19 -0700

    os.O_WRONLY

--- unneeded_stuff/test_fishSpeak_pipe.py
+++ unneeded_stuff/test_fishSpeak_pipe.py
@@ -2,7 +2,7 @@ import os
 
 FIFO_PATH = '/tmp/SayThis_Fish'
 
-pipeout = os.open(FIFO_PATH,"w")
+pipeout = os.open(FIFO_PATH,os.O_WRONLY)
 
 saythis = "hello, I am a fish"
 os.write(pipeout, saythis)

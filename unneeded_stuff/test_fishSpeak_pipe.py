d519684b4c3d8199aacc3487856d73c6c3f330b5
Mark Niemann-Ross <mark.niemannross@gmail.com>
2016-07-06 21:36:07 -0700

    bytes instead of str

--- unneeded_stuff/test_fishSpeak_pipe.py
+++ unneeded_stuff/test_fishSpeak_pipe.py
@@ -5,5 +5,5 @@ FIFO_PATH = '/tmp/SayThis_Fish'
 pipeout = os.open(FIFO_PATH,os.O_WRONLY)
 
 saythis = "hello, I am a fish"
-os.write(pipeout, saythis)
+os.write(pipeout, bytes(saythis, 'UTF-8'))
 os.close(pipeout)

ca7ad202b60a67fcf68d3f39b0edaccfd164e8c7
Mark Niemann-Ross <mark.niemannross@gmail.com>
2016-07-06 21:40:32 -0700

    added newline

--- unneeded_stuff/test_fishSpeak_pipe.py
+++ unneeded_stuff/test_fishSpeak_pipe.py
@@ -4,5 +4,5 @@ FIFO_PATH = '/tmp/SayThis_Fish'
 
 pipeout = os.open(FIFO_PATH,os.O_WRONLY)
 
-os.write(pipeout, bytes("hello, I am a fish", 'UTF-8'))
+os.write(pipeout, bytes("hello, I am a fish\n", 'UTF-8'))
 os.close(pipeout)

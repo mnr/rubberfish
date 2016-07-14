62157ad337b42a15b4c43a0f0e25c3a5f7be29e3
Mark Niemann-Ross <mark.niemannross@gmail.com>
2016-07-14 09:04:19 -0700

    added the HEAT control
    power is now controlled through pushbutton on inside right. Above power
    supply. This frees up the three switches on the front. Also changed
    get_boxVent to get_boxVENT to correspond with the other names

--- software/box_controls.py
+++ software/box_controls.py
@@ -3,6 +3,7 @@
 |---|---|---|---|---|
 | 12 | 1 | 18 | in | vent (switch) |
 | 16 | 4 | 23 | in | light (switch) |
+| 10 | ? | ?? | in | heat (switch) |
 """
 
 import RPi.GPIO as GPIO
@@ -13,15 +14,20 @@ class boxControls:
     # GPIO pins assigned to the two front-panel switches
     boxVENT = 12
     boxLIGHT = 16
+    boxHEAT = 10
 
     def __init__(self):
 
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.boxVENT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(self.boxLIGHT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.boxHEAT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-    def get_boxVent_STATE(self):
+    def get_boxVENT_STATE(self):
         return GPIO.input(self.boxVENT)
 
     def get_boxLIGHT_STATE(self):
         return GPIO.input(self.boxLIGHT)
+
+    def get_boxHEAT_STATE(self):
+        return GPIO.input(self.boxHEAT)

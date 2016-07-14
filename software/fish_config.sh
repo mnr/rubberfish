729e8b4d31dc8a9e6bd85e8e8fcd12adf976668d
Mark Niemann-Ross <mark.niemannross@gmail.com>
2016-07-14 16:17:39 -0700

    problems with gain?

--- software/fish_config.sh
+++ software/fish_config.sh
@@ -43,7 +43,9 @@ case "$1" in
     # http://sox.sourceforge.net/
     # set up the audiodev so sox know where to look
     export AUDIODEV=hw:1,0
-    rec /home/pi/rubberfish/sounds/snd.wav silence 1 .5 2.85% 1 1.0 3.0% vad gain -l 9 −−no−show−progress : newfile : restart & #best so far
+    # the following produces "rec FAIL gain: usage: [-e|-b|-B|-r] [-n] [-l|-h] [gain-dB]"
+    # rec /home/pi/rubberfish/sounds/snd.wav silence 1 .5 2.85% 1 1.0 3.0% vad gain -n −−no−show−progress : newfile : restart & #best so far
+    rec /home/pi/rubberfish/sounds/snd.wav silence 1 .5 2.85% 1 1.0 3.0% vad −−no−show−progress : newfile : restart & 
 
     echo "##########"
     echo "Finished with Initial Fish"

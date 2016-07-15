793c7494b7de7004f8077d45b75dc40394a1a3ed
Mark Niemann-Ross <mark.niemannross@gmail.com>
2016-07-15 15:03:50 -0700

    testing switches

--- software/__main__.py
+++ software/__main__.py
@@ -15,6 +15,8 @@ def main(args=None):
 
     GPIO.add_event_detect(my_box.boxVENT, GPIO.BOTH)
     GPIO.add_event_detect(my_box.boxLIGHT, GPIO.BOTH)
+    GPIO.add_event_detect(my_box.boxHEAT, GPIO.BOTH)
+
 
 
     try:
@@ -26,15 +28,15 @@ def main(args=None):
             # localtime = time.asctime( time.localtime(time.time()) )
             # my_fish.speak("hello, Dave, the time is " + localtime)
 
-            sidewalkEnds = "Where the Sidewalk Ends by Shel Silverstein. There is a place where the sidewalk ends. And before the street begins,. And there the grass grows soft and white, And there the sun burns crimson bright, And there the moon-bird rests from his flight To cool in the peppermint wind. Let us leave this place where the smoke blows black And the dark street winds and bends. Past the pits where the asphalt flowers grow We shall walk with a walk that is measured and slow, And watch where the chalk-white arrows go To the place where the sidewalk ends. Yes we'll walk with a walk that is measured and slow, And we'll go where the chalk-white arrows go, For the children, they mark, and the children, they know The place where the sidewalk ends. "
-            my_fish.speak(sidewalkEnds)
+            # sidewalkEnds = "Where the Sidewalk Ends by Shel Silverstein. There is a place where the sidewalk ends. And before the street begins,. And there the grass grows soft and white, And there the sun burns crimson bright, And there the moon-bird rests from his flight To cool in the peppermint wind. Let us leave this place where the smoke blows black And the dark street winds and bends. Past the pits where the asphalt flowers grow We shall walk with a walk that is measured and slow, And watch where the chalk-white arrows go To the place where the sidewalk ends. Yes we'll walk with a walk that is measured and slow, And we'll go where the chalk-white arrows go, For the children, they mark, and the children, they know The place where the sidewalk ends. "
+            # my_fish.speak(sidewalkEnds)
 
             # sleep(.5)
             # my_fish.head(enthusiasm=randomEnthusiasm,fishDuration=randomDuration)
             # sleep(1)
             # my_fish.mouth(enthusiasm=randomEnthusiasm,fishDuration=randomDuration)
             # sleep(1)
-            print('vent:{ventValue}, light:{lightValue}.'.format(ventValue=my_box.get_boxVent_STATE(), lightValue=my_box.get_boxLIGHT_STATE()))
+            print('vent:{ventValue}, light:{lightValue}, heat:{heatValue}.'.format(ventValue=my_box.get_boxVENT_STATE(), lightValue=my_box.get_boxLIGHT_STATE(), heatValue=my_box.get_boxHEAT_STATE()))
             if GPIO.event_detected(my_box.boxVENT):
                 if my_box.get_boxVENT_STATE():
                     print ("VENT has been thrown to the left")

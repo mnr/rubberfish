9adacb8031ed133afd3ee7124d3196e387c083da
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-01-07 15:27:05 -0800

    Test of Bing Speech

---
+++ tests/bingTTS.py
@@ -0,0 +1,64 @@
+# from  https://github.com/Microsoft/Cognitive-Speech-TTS/blob/master/Samples-Http/Python/TTSSample.py
+
+###
+#Copyright (c) Microsoft Corporation
+#All rights reserved.
+#MIT License
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+###
+import http.client, urllib.parse, json
+
+#Note: The way to get api key:
+#Free: https://www.microsoft.com/cognitive-services/en-us/subscriptions?productId=/products/Bing.Speech.Preview
+#Paid: https://portal.azure.com/#create/Microsoft.CognitiveServices/apitype/Bing.Speech/pricingtier/S0
+apiKey = "something here"
+
+params = ""
+headers = {"Ocp-Apim-Subscription-Key": apiKey}
+
+#AccessTokenUri = "https://api.cognitive.microsoft.com/sts/v1.0/issueToken";
+AccessTokenHost = "api.cognitive.microsoft.com"
+path = "/sts/v1.0/issueToken"
+
+# Connect to server to get the Access Token
+print ("Connect to server to get the Access Token")
+conn = http.client.HTTPSConnection(AccessTokenHost)
+conn.request("POST", path, params, headers)
+response = conn.getresponse()
+print(response.status, response.reason)
+
+data = response.read()
+conn.close()
+
+accesstoken = data.decode("UTF-8")
+print ("Access Token: " + accesstoken)
+
+openSpeak = "<speak version='1.0' xml:lang='en-us'><voice xml:lang='en-us' xml:gender='Female' name='Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)'>"
+closeSpeak = "</voice></speak>"
+sayThis = "Bing speech for python."
+
+body = openSpeak + sayThis + closeSpeak
+
+headers = {"Content-type": "application/ssml+xml",
+			"X-Microsoft-OutputFormat": "riff-16khz-16bit-mono-pcm",
+			"Authorization": "Bearer " + accesstoken,
+			"X-Search-AppId": "07D3234E49CE426DAA29772419F436CA",
+			"X-Search-ClientID": "1ECFAE91408841A480F00935DC390960",
+			"User-Agent": "TTSForPython"}
+
+#Connect to server to synthesize the wave
+print ("\nConnect to server to synthesize the wave")
+conn = http.client.HTTPSConnection("speech.platform.bing.com")
+conn.request("POST", "/synthesize", body, headers)
+response = conn.getresponse()
+print(response.status, response.reason)
+
+data = response.read()
+conn.close()
+print("The synthesized wave length: %d" %(len(data)))
+
+outfile = open('spokenFile.wav','wb')
+outfile.write(data)
+outfile.close()

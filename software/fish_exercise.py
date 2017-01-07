e6db92dbec3bd099a329768ac728b9974c70c3bd
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-01-07 15:27:39 -0800

    add fishSays

--- software/fish_exercise.py
+++ software/fish_exercise.py
@@ -21,6 +21,10 @@ my_fish.tail(fishDuration=.4,enthusiasm=75)
 print('tail end')
 time.sleep(2)
 
+# Speech
+print('speaking')
+my_fish.fishSays("Hello World. It's nice to be here")
+
 # voltage meter
 print('swing voltage needle')
 for setVoltage in range(255):

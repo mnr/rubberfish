9e5d9e1ef1783f1703e38dcc8160c178ef98a0e8
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-01-07 21:59:49 -0800

    integrated bing tts
    as experimented in bingTTS.py

--- software/bmbb_fish.py
+++ software/bmbb_fish.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 
+###
+# portions related to Bing Text-to-speech are Copyright (c) Microsoft Corporation
+#All rights reserved.
+#MIT License
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+###
+
 # rpi.gpio documentation at https://sourceforge.net/p/raspberry-gpio-python/wiki/
 import RPi.GPIO as GPIO
 from time import sleep as sleep
 import logging
 import threading
+import http.client, urllib.parse, json # supports Bing Text-to-speech
+import pygame # used to play back the speech file
 
 
 class BmBB:
@@ -16,6 +27,21 @@ class BmBB:
     fishHEAD = 7
     # fishHEAD_reverse = 15
     fishMotorEnable = 18
+
+    # Bing TTS variables
+    apiKey = "" # This won't work unless you get the api key from https://www.microsoft.com/cognitive-services/en-us/subscriptions?productId=/products/Bing.Speech.Preview
+    getAccessParams = ""
+    getAccessHeaders = {"Ocp-Apim-Subscription-Key": apiKey}
+
+    #AccessTokenUri = "https://api.cognitive.microsoft.com/sts/v1.0/issueToken";
+    AccessTokenHost = "api.cognitive.microsoft.com"
+    getAccessPath = "/sts/v1.0/issueToken"
+
+    openSpeak = "<speak version='1.0' xml:lang='en-us'><voice xml:lang='en-us' xml:gender='Female' name='Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)'>"
+    closeSpeak = "</voice></speak>"
+
+    synthWaveHeaders = None
+
 
     # other variables
     PWMstatus = None #declaring PWMstatus here for later assignment
@@ -44,6 +70,30 @@ class BmBB:
         hdlr.setFormatter(formatter)
         self.logger.addHandler(hdlr)
         self.logger.setLevel(logging.DEBUG)
+
+        # set up Bing Text-to-speech
+        # Connect to server to get the Access Token
+        print ("Connect to server to get the Access Token")
+        conn = http.client.HTTPSConnection(self.AccessTokenHost)
+        conn.request("POST", self.getAccessPath, self.getAccessParams, self.getAccessHeaders)
+        response = conn.getresponse()
+        print(response.status, response.reason)
+
+        apiKeyData = response.read()
+        conn.close()
+
+        accesstoken = apiKeyData.decode("UTF-8")
+        print ("Access Token: " + accesstoken)
+        self.synthWaveHeaders = {"Content-type": "application/ssml+xml",
+        			"X-Microsoft-OutputFormat": "riff-16khz-16bit-mono-pcm",
+        			"Authorization": "Bearer " + accesstoken,
+        			"X-Search-AppId": "07D3234E49CE426DAA29772419F436CA",
+        			"X-Search-ClientID": "1ECFAE91408841A480F00935DC390960",
+        			"User-Agent": "TTSForPython"}
+
+        # set up pygame
+        pygame.mixer.pre_init(4000,-16,2,2048)
+        pygame.mixer.init()
 
         # do something to indicate life
         self.head()
@@ -108,7 +158,6 @@ class BmBB:
         self.logger.info('tailBack: No Parameters')
         GPIO.output(self.fishTAIL,GPIO.LOW)
 
-
     def adjustPWM(self,PWMDutyCycle=50):
         # where 0.0 <= PWMDutyCycle <= 100.0
         PWMDutyCycle = 100 if PWMDutyCycle > 100 else PWMDutyCycle
@@ -116,4 +165,20 @@ class BmBB:
         self.PWMstatus.ChangeDutyCycle(PWMDutyCycle)
 
     def fishSays(self,phraseToSay="Hello World"):
-        pass
+        synthWaveBody = self.openSpeak + phraseToSay + self.closeSpeak
+
+        #Connect to server to synthesize the wave
+        print ("\nConnect to server to synthesize the wave")
+        conn = http.client.HTTPSConnection("speech.platform.bing.com")
+        conn.request("POST", "/synthesize", synthWaveBody, self.synthWaveHeaders)
+        response = conn.getresponse()
+        print(response.status, response.reason)
+
+        synthWaveData = response.read()
+        conn.close()
+        print("The synthesized wave length: %d" %(len(synthWaveData)))
+        print("playing sound")
+        asound = pygame.mixer.Sound(synthWaveData)
+        channel = asound.play()
+        while channel.get_busy() == True:
+            continue

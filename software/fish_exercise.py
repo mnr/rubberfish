f22edfa07ee7fdd72e9c0e90ebd5ec728d983822
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-01-09 18:35:18 -0800

    Update fish_exercise.py

--- software/fish_exercise.py
+++ software/fish_exercise.py
@@ -11,8 +11,8 @@ my_box = boxControls()
 
 # Speech
 print('speaking')
-my_fish.fishSays("Hello World. It's nice to be here")
-sayTheTime = "The time is" + time.strftime("%A, %B %d, %H %M",time.localtime())
+my_fish.fishSays("Hello World. It is nice to be here")
+sayTheTime = "The time is " + time.strftime("%A, %B %d, %H %M",time.localtime())
 my_fish.fishSays(sayTheTime)
 
 # head

bf63cfed5fb6f36c01d6ed62355e93ee73ca4c07
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-01-17 20:47:03 -0800

    added logger to callback

--- software/LightVentHeatirq.py
+++ software/LightVentHeatirq.py
@@ -9,6 +9,18 @@ runs in background. Started in fish_config.sh
 
 from switch_obj import boxSwitch
 from bmbb_fish import BmBB
+import logging
+
+
+# set up error logging
+logger = None #declaring logger here for later use
+
+logger = logging.getLogger('FishControl')
+hdlr = logging.FileHandler('/var/tmp/fish.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s %(thread)d %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.DEBUG)
 
 my_fish = BmBB()
 
@@ -17,6 +29,8 @@ def HEAT_callback(GPIOpin):
     switch_status = "on" if switchHeat.get_state() else "off"
     print("The heat switch was turned ",switch_status)
     my_fish.fishSays("The heat switch was turned " + switch_status)
+    logger.info("The heat switch was turned " + switch_status)
+
 
 switchHeat = boxSwitch(boxHEAT)
 switchHeat.set_callback(HEAT_callback)

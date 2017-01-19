ab2bdf2e2f9529062f902c6d16ff1d2636680302
MNR at the Fish <mark.niemannross@gmail.com>
2017-01-19 12:51:56 -0800

    add check_same_thread to sqlite access

--- software/bmbb_fish.py
+++ software/bmbb_fish.py
@@ -52,7 +52,7 @@ class BmBB:
         self.logger.setLevel(logging.DEBUG)
 
         # set up SQLite
-        self.dbconnect = sqlite3.connect("/home/pi/rubberfish/textToSpeech.db")
+        self.dbconnect = sqlite3.connect("/home/pi/rubberfish/textToSpeech.db", check_same_thread=False)
         self.cursor = self.dbconnect.cursor()
 
         # do something to indicate life

3a13ad743df8044756068eaf459a2086d612565b
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-01-20 12:55:25 -0800

    added handlers for vent and light

--- software/LightVentHeatirq.py
+++ software/LightVentHeatirq.py
@@ -24,6 +24,8 @@ logger.setLevel(logging.DEBUG)
 
 my_fish = BmBB()
 
+#######################
+# Setting up the Heat Switch
 boxHEAT = 10
 def HEAT_callback(GPIOpin):
     switchState,switchChanged = switchHeat.get_state()
@@ -35,9 +37,32 @@ def HEAT_callback(GPIOpin):
 switchHeat = boxSwitch(boxHEAT)
 switchHeat.set_callback(HEAT_callback)
 
+#######################
+# Setting up the Light Switch
 boxLIGHT = 16
+def LIGHT_callback(GPIOpin):
+    switchState,switchChanged = switchLight.get_state()
+    if switchChanged:
+        switch_status = "on" if switchState else "off"
+        my_fish.fishSays("The light switch was turned " + switch_status)
+        logger.info("The Light switch was turned " + switch_status)
 
+switchLight = boxSwitch(boxLIGHT)
+switchLight.set_callback(LIGHT_callback)
+
+#######################
+# Setting up the Vent Switch
 boxVENT = 12
+def VENT_callback(GPIOpin):
+    switchState,switchChanged = switchVent.get_state()
+    if switchChanged:
+        switch_status = "on" if switchState else "off"
+        my_fish.fishSays("The Vent switch was turned " + switch_status)
+        logger.info("The Vent switch was turned " + switch_status)
 
+switchVent = boxSwitch(boxVENT)
+switchVent.set_callback(VENT_callback)
+
+##### Keep the shell alive
 while True:
     pass # loop to keep the python shell open

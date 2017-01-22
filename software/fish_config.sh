dcb75bee96a40d0be340637b666891a050450e07
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-01-21 17:16:21 -0800

    add fish_head_tail to background

--- software/fish_config.sh
+++ software/fish_config.sh
@@ -31,6 +31,7 @@ case "$1" in
     python3 /home/pi/rubberfish/software/do_TTS.py &
     python3 /home/pi/rubberfish/software/speakNextPhrase.py &
     python3 /home/pi/rubberfish/software/LightVentHeatirq.py &
+    python3 /home/pi/rubberfish/software/fish_head_tail.py &
 
     # Finished with Initial Fish"
 

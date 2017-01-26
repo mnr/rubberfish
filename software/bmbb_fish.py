c921364532b354cf2328d41fc9a45a444d3f9368
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-01-26 11:58:08 -0800

    testing for presence of punk

--- software/bmbb_fish.py
+++ software/bmbb_fish.py
@@ -58,8 +58,10 @@ class BmBB:
         self.cursor = self.dbconnect.cursor()
 
         # set up nltk
-        nltk.download('punkt')
-
+        try:
+            nltk.data.find('punkt.zip')
+        except LookupError:
+            nltk.download('punkt')
 
     def shut_down_fish(self):
         self.logger.info('killing the fish')
